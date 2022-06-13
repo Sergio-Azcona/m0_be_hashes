7f54c727484ae2e29e977a75e5d79fc379fa8b51
Sergio Azconan <sergio.j.azcona@gmail.com>
2022-06-12 23:07:31 -0400

    Adding files

--- hashes_2.rb
+++ hashes_2.rb
@@ -6,12 +6,12 @@ states = {
   "New Jersey" => "NJ",
   "Colorado" => "CO"
 }
+states.each do |name, short|
+  p "the abbreviation for #{name} is #{short}"
+end
 
 # Use the each method to iterate through it and print out a sentence stating each state name and abbreviation
-#
-#
-#
-
+p states
 
 # ======================================
 # 2- Using the following Hash
@@ -22,11 +22,12 @@ birthdays = {
   "Amos" => "July 21",
   "Jeff" => "February 12"
 }
+birthdays.each do |friend, bd|
+  p "#{friend} birthday is #{bd}"
+end
 
 # Use the each method to iterate through it and print out a sentence stating each friends name and birthday
-#
-#
-#
+birthdays
 
 
 # ======================================
@@ -38,8 +39,11 @@ login_statuses = {
   "Amos" => true,
   "Jeff" => true
 }
-
+login_statuses.each do |yes|
+  if login_statuses.values == true
+    p "party"
+  end
+end
 # Use the each method to iterate through it and print ONLY the names of friends who are "logged in"
-#
-#
-#
+p login_statuses
+#the if statment within the each method did not work...
--- self_eval.md
+++ self_eval.md
@@ -1,5 +1,9 @@
 # Self Evaluation
 
 - What did you struggle with in this section?
+i could not get the last Q in the second hashes within the given time (15 minutes) to work. also, the Array + Hashes combo i will need to practice
 - How is note-taking going? What are you finding helpful? When do you find yourself taking the most helpful notes?
+it has gone pretty well. i like digital notes as it is easier to search ( cmd + F) ... that being said, it is easier for info to get lost when it is electronically
+
 - What do you lean into - Googling, or trying to solve a problem in code on your own first?
+i try to solve it on my own. i do google but have found that there are a ton of ways thrown at you...and i do not know which are trustworthy yet
